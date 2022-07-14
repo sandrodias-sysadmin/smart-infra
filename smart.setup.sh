9b68d3eb89ffb400c933aac1627380dc31f90643
Sandro Dias <sandrodias.oficiall@gmail.com>
2022-07-14 14:02:46 -0300

    Installer creation

---
+++ smart.setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Projeto: Smart-Infra é uma coleção de scripts em Bash para automação de infraestrutura de rede em linux.
+# Hospedado:  https://github.com/sandrodias-sysadmin/smart-infra
+# Autor: Sandro Dias
+# E-mail: sandrodias@gmail.com
+#
+# Caminho Absoluto: /smart-infra/install-smart-infra.sh
+# Função: Levantar informações cruciais sobre o sistema, rede e etc guardando-as em variáveis
+# Atualizado em: 29/03/2022
+# Versao: 0.1
+#
+#
+##############################
